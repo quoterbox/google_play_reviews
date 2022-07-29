a26e01f18d8466b198b03d563c8511af7c3829ee
John_Go <jooqeer@yandex.ru>
2022-07-29 21:23:28 +0300

    Total refactoring, made useful class for scrapping

--- main.py
+++ main.py
@@ -1,230 +1,98 @@
 import csv
-import time
-import random
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException
+from scrapper.gpr_scrapper import GPRScrapper
+
 
 driver = webdriver.Chrome()
 
-# google_play_review_scrapper = GPRScrapper(
-#     driver,
-#     {
-#         "language": "ru", #en
-#         "review_link_xpath": "//h2[contains(text(), 'Оценки и отзывы')]/../../../../div//i[contains(text(), 'arrow_forward')]",
-#         "xpath_setup":{
-#             "first_review_xpath": {
-#                 "self": "",
-#                 "name": "",
-#                 "date": "",
-#                 "rating": "",
-#                 "helpful_count": "",
-#                 "text": "",
-#             },
-#             "second_review": {
-#                 "xpath": "",
-#                 "name": "",
-#                 "date": "",
-#                 "rating": "",
-#                 "helpful_count": "",
-#                 "text": "",
-#             },
-#         },
-#         "output_file": {
-#             "name": "reviews.csv",
-#             "fields": {
-#                 "name": "Name",
-#                 "date": "Date",
-#                 "rating": "Rating",
-#                 "helpful_count": "Helpful reviews count",
-#                 "text": "Text",
-#             }
-#         }
-#     }
-# )
-#
-# app_links = [
-#     "https://play.google.com/store/apps/details?id=com.coinbase.android&hl=ru",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.coinbase.android",
-#     "https://play.google.com/store/apps/details?hl=en&id=org.toshi",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.binance.dev",
-#     "https://play.google.com/store/apps/details?hl=en&id=co.mona.android",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.bybit.app",
-#     "https://play.google.com/store/apps/details?hl=en&id=io.metamask",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.wallet.crypto.trustapp",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.nexowallet",
-#     "https://play.google.com/store/apps/details?hl=en&id=br.com.mercadobitcoin.android",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.nicehash.metallum",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.whitebit.android",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.coinsbit.coinsbit",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.wrx.wazirx",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.coindcx.btc",
-#     "https://play.google.com/store/apps/details?hl=en&id=id.co.bitcoin",
-#     "https://play.google.com/store/apps/details?hl=en&id=co.bitx.android.wallet",
-#     "https://play.google.com/store/apps/details?hl=en&id=pro.huobi",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.digifinex.app",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.gateio.gateio",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.stormgain.mobile",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.mexcpro.client",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.app.xt",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.kubi.kucoin",
-#     "https://play.google.com/store/apps/details?hl=en&id=com.blockfolio.blockfolio"
-# ]
-#
-# google_play_review_scrapper.run(app_links)
-# driver.close()
+google_play_review_scrapper = GPRScrapper(
+    driver,
+    {
+        # 0 - maximum
+        "count_reviews": 10,
+        # en/ru
+        "language": "en",
+        # you should copy these xpaths from your browser (right click and select "Inspect element")
+        "xpath_options": {
+            "app_name_xpath": "/html/body/c-wiz[2]/div/div/div[1]/div[1]/div/div/c-wiz/div[2]/div[1]/div/h1",
+            "review_link_xpath": "/html/body/c-wiz[2]/div/div/div[1]/div[2]/div/div[1]/c-wiz[4]/section/header/div/div[2]/button/i",
+            "modal_window_name_xpath": "/html/body/div[4]/div[2]/div/div/div/div/div[1]/div/div/h5",
+            "first_review": {
+                "review_body": "/html/body/div[4]/div[2]/div/div/div/div/div[2]/div/div[1]/div[1]",
+                "name": "/html/body/div[4]/div[2]/div/div/div/div/div[2]/div/div[1]/div[1]/header/div[1]/div[1]/div",
+                "date": "/html/body/div[4]/div[2]/div/div/div/div/div[2]/div/div[1]/div[1]/header/div[2]/span",
+                "rating": "/html/body/div[4]/div[2]/div/div/div/div/div[2]/div/div[1]/div[1]/header/div[2]/div",
+                "helpful_count": "/html/body/div[4]/div[2]/div/div/div/div/div[2]/div/div[1]/div[1]/div[2]/div",
+                "text": "/html/body/div[4]/div[2]/div/div/div/div/div[2]/div/div[1]/div[1]/div[1]",
+            },
+            "second_review": {
+                "review_body": "/html/body/div[4]/div[2]/div/div/div/div/div[2]/div/div[1]/div[2]",
+            },
+        },
+        # time delays will be randomly selected between min and max value
+        "time_options": {
+            "delay_before_open_modal": [5, 10],
+            "delay_before_close": [5, 10],
+            "delay_between_review": [1, 3]
+        },
+        # pixels offsets will be randomly selected between min and max value
+        "scroll_options": {
+            "scroll_origin_x_offset": [0, 50],
+            "scroll_origin_y_offset": [0, 50],
+            "scroll_delta_x": [0, 50],
+            "scroll_delta_y": [0, 50],
+        }
+    }
+)
 
+# links for example
+google_play_review_scrapper.run([
+    "https://play.google.com/store/apps/details?hl=en&id=com.coinbase.android",
+    "https://play.google.com/store/apps/details?hl=en&id=org.toshi",
+    "https://play.google.com/store/apps/details?hl=en&id=com.binance.dev",
+    "https://play.google.com/store/apps/details?hl=en&id=co.mona.android",
+    "https://play.google.com/store/apps/details?hl=en&id=com.bybit.app",
+    "https://play.google.com/store/apps/details?hl=en&id=io.metamask",
+    "https://play.google.com/store/apps/details?hl=en&id=com.wallet.crypto.trustapp",
+    "https://play.google.com/store/apps/details?hl=en&id=com.nexowallet",
+    "https://play.google.com/store/apps/details?hl=en&id=br.com.mercadobitcoin.android",
+    "https://play.google.com/store/apps/details?hl=en&id=com.nicehash.metallum",
+    "https://play.google.com/store/apps/details?hl=en&id=com.whitebit.android",
+    "https://play.google.com/store/apps/details?hl=en&id=com.coinsbit.coinsbit",
+    "https://play.google.com/store/apps/details?hl=en&id=com.wrx.wazirx",
+    "https://play.google.com/store/apps/details?hl=en&id=com.coindcx.btc",
+    "https://play.google.com/store/apps/details?hl=en&id=id.co.bitcoin",
+    "https://play.google.com/store/apps/details?hl=en&id=co.bitx.android.wallet",
+    "https://play.google.com/store/apps/details?hl=en&id=pro.huobi",
+    "https://play.google.com/store/apps/details?hl=en&id=com.digifinex.app",
+    "https://play.google.com/store/apps/details?hl=en&id=com.gateio.gateio",
+    "https://play.google.com/store/apps/details?hl=en&id=com.stormgain.mobile",
+    "https://play.google.com/store/apps/details?hl=en&id=com.mexcpro.client",
+    "https://play.google.com/store/apps/details?hl=en&id=com.app.xt",
+    "https://play.google.com/store/apps/details?hl=en&id=com.kubi.kucoin",
+    "https://play.google.com/store/apps/details?hl=en&id=com.blockfolio.blockfolio"
+])
 
+output_file = {
+    "name": "reviews.csv",
+    "fields": {
+        "app_name": "App name",
+        "app_link": "App link",
+        "name": "Name",
+        "date": "Date",
+        "rating": "Rating",
+        "helpful_count": "Helpful reviews count",
+        "text": "Text",
+    }
+}
 
-driver.get("https://play.google.com/store/apps/details?hl=ru&id=com.coinbase.android&gl=US")#&gl=RU
+with open(output_file["name"], "w", encoding='utf-8', newline='') as file:
+    writer = csv.DictWriter(file, delimiter=';', fieldnames=output_file["fields"].keys())
+    writer.writerow(output_file["fields"])
 
-review_link_xpath = "//h2[contains(text(), 'Оценки и отзывы')]/../../../../div//i[contains(text(), 'arrow_forward')]"
-review_link = driver.find_element(By.XPATH, review_link_xpath)
+with open(output_file["name"], "a", encoding='utf-8', newline='') as file:
+    writer = csv.DictWriter(file, delimiter=';', fieldnames=output_file["fields"].keys())
+    for review in google_play_review_scrapper.get_reviews():
+        writer.writerow(review)
 
-time.sleep(random.randint(1, 3))
-
-scroll_to_reviews = ScrollOrigin(review_link, 0, 0)
-
-actions = ActionChains(driver)
-actions.scroll_to_element(review_link).perform()
-actions.scroll_from_origin(scroll_to_reviews, 0, 500)
-actions.move_to_element(review_link)
-actions.click(review_link)
-actions.perform()
-
-modal_window = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div[2]/div/div/div/div/div[2]/div/div[1]")))
-
-review_number = 1
-main_xpath = "html/body/div[4]/div[2]/div/div/div/div/div[2]/div/div[1]/div"
-reviews = []
-
-# fieldnames = ['Name', 'Date', 'Rating', 'Helpful count', 'Text']
-fieldnames = ['name', 'date', 'rating', 'helpful_count', 'text']
-
-
-with open("out.csv", "w", encoding='utf-8', newline='') as file:
-    writer = csv.DictWriter(file, delimiter=';', fieldnames=fieldnames)
-    writer.writerow({
-        'name': 'Name',
-        'date': 'Date',
-        'rating': 'Rating',
-        'helpful_count': 'Helpful count',
-        'text': 'Text',
-    })
-
-while True:
-        review_element = WebDriverWait(driver, 30).until(
-                    EC.presence_of_element_located(
-                        (By.XPATH, "%s[%d]" % (main_xpath, review_number))))
-
-        if review_element:
-
-            name = ""
-            date = ""
-            rating = ""
-            helpful_count = ""
-            text = ""
-
-            print("Review num-%d has been found" % review_number)
-
-            scroll_to_review = ScrollOrigin(review_element, 0, random.randint(100, 200))
-            actions.scroll_from_origin(scroll_to_review, 0, 500).move_to_element(review_element).perform()
-
-            # name = driver.find_element(By.XPATH, "%s[%d]/header/div[1]/div[1]" % (main_xpath, review_number)).text
-            try:
-                name = driver.find_element(By.XPATH, "%s[%d]/header/div[1]/div[1]" % (main_xpath, review_number)).text
-                print("Name: %s" % name)
-            except NoSuchElementException:
-                print("ELEMENT -name- has not been found!")
-                pass
-
-            # name = WebDriverWait(driver, 10).until(
-            #     EC.presence_of_element_located(
-            #         (By.XPATH, "%s[%d]/header/div[1]/div[1]" % (main_xpath, review_number)))).text
-            # print("Name: %s" % name)
-
-
-            try:
-                date = driver.find_element(By.XPATH, "%s[%d]/header/div[2]/span" % (main_xpath, review_number)).text
-                print("Date: %s" % date)
-            except NoSuchElementException:
-                print("ELEMENT -date- has not been found!")
-                pass
-
-            # date = driver.find_element(By.XPATH, "%s[%d]/header/div[2]/span" % (main_xpath, review_number)).text
-            # date = WebDriverWait(driver, 10).until(
-            #     EC.presence_of_element_located(
-            #         (By.XPATH, "%s[%d]/header/div[2]/span" % (main_xpath, review_number)))).text
-            # print("Date: %s" % date)
-
-
-            try:
-                rating = driver.find_element(By.XPATH, "%s[%d]/header/div[2]/div" % (main_xpath, review_number)).get_attribute("aria-label")
-                print("Rating: %s" % rating)
-            except NoSuchElementException:
-                print("ELEMENT -rating- has not been found!")
-                pass
-
-            # rating = driver.find_element(By.XPATH, "%s[%d]/header/div[2]/div" % (main_xpath, review_number)).get_attribute("aria-label")
-            # rating = WebDriverWait(driver, 10).until(
-            #     EC.presence_of_element_located(
-            #         (By.XPATH, "%s[%d]/header/div[2]/div" % (main_xpath, review_number)))).get_attribute("aria-label")
-            # print("Rating: %s" % rating)
-
-
-            try:
-                helpful_count = driver.find_element(By.XPATH, "%s[%d]/div[2]/div" % (main_xpath, review_number)).text
-                print("Helpful count: %s" % helpful_count)
-            except NoSuchElementException:
-                print("ELEMENT -helpful_count- has not been found!")
-                pass
-
-            # helpful_count = driver.find_element(By.XPATH, "%s[%d]/div[2]/div" % (main_xpath, review_number)).text
-            # helpful_count = WebDriverWait(driver, 10).until(
-            #     EC.presence_of_element_located(
-            #         (By.XPATH, "%s[%d]/div[2]/div" % (main_xpath, review_number)))).text
-            # print("Helpful count: %s" % helpful_count)
-
-
-            try:
-                text = driver.find_element(By.XPATH, "%s[%d]/div[1]" % (main_xpath, review_number)).text
-                print("Text: %s" % text)
-            except NoSuchElementException:
-                print("ELEMENT -text- has not been found!")
-                pass
-
-            # text = driver.find_element(By.XPATH, "%s[%d]/div[1]" % (main_xpath, review_number)).text
-            # text = WebDriverWait(driver, 10).until(
-            #     EC.presence_of_element_located(
-            #         (By.XPATH, "%s[%d]/div[2]/div" % (main_xpath, review_number)))).text
-            # print("Text: %s" % text)
-
-            with open("out.csv", "a", encoding='utf-8', newline='') as file:
-                writer = csv.DictWriter(file, delimiter=';', fieldnames=fieldnames)
-                writer.writerow({
-                    'name': name,
-                    'date': date,
-                    'rating': rating,
-                    'helpful_count': helpful_count,
-                    'text': text,
-                })
-
-            time.sleep(random.randint(1, 3))
-
-            review_number = review_number + 1
-
-        else:
-
-            print("Review num-%d has not been found!" % review_number)
-            break
-
-
-
-time.sleep(random.randint(4, 7))
-
-driver.close()
+print("Reviews has been successfully saved to file!")
--- scrapper/gpr_scrapper.py
+++ scrapper/gpr_scrapper.py
@@ -1,9 +1,226 @@
+import re
+import time
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
+
 
 class GPRScrapper:
+    __review_num = 1
+    __main_xpath = ""
 
+    reviews = []
 
+    # 0 - maximum
+    count_reviews = 10,
 
+    language = "en"
+    xpath_options = {
+        "app_name_xpath": "",
+        "review_link_xpath": "",
+        "modal_window_name_xpath": "",
+        "first_review": {
+            "review_body": "",
+            "name": "",
+            "date": "",
+            "rating": "",
+            "helpful_count": "",
+            "text": "",
+        },
+        "second_review": {
+            "review_body": "",
+        },
+    }
+    regexp_xpath_options = {
+        "review_body": "",
+        "name": "",
+        "date": "",
+        "rating": "",
+        "helpful_count": "",
+        "text": "",
+    }
+    time_options = {
+        "delay_before_open_modal": [5, 10],
+        "delay_before_close": [5, 10],
+        "delay_between_review": [1, 3]
+    }
+    scroll_options = {
+        "scroll_origin_x_offset": [0, 50],
+        "scroll_origin_y_offset": [0, 50],
+        "scroll_delta_x": [0, 50],
+        "scroll_delta_y": [0, 50],
+    }
+
+    def __init__(self, driver: webdriver, options: {}):
+        self.driver = driver
+        self.actions = ActionChains(self.driver)
+        self.__set_count_reviews(options["count_reviews"])
+        self.__set_language(options["language"])
+        self.__set_xpath_options(options["xpath_options"])
+        self.__set_time_options(options["time_options"])
+        self.__set_scroll_options(options["scroll_options"])
+
+        self.__main_xpath = self.__find_main_xpath(
+            options["xpath_options"]["first_review"]["review_body"],
+            options["xpath_options"]["second_review"]["review_body"]
+        )
+
+        self.__set_regexp_xpath_options(self.__main_xpath, options["xpath_options"]["first_review"])
+
+    def __set_regexp_xpath_options(self, main_xpath: str, first_review: {}):
+        for key, value in first_review.items():
+            self.regexp_xpath_options[key] = value.replace(main_xpath, "%s")
+            self.regexp_xpath_options[key] = re.sub(r'(%s)(\[\d+])(.*)', r'\1[%d]\3', self.regexp_xpath_options[key])
+
+    def __set_count_reviews(self, count_reviews: int):
+        self.count_reviews = count_reviews
+
+    def __set_language(self, language: str):
+        self.language = language
+
+    def __set_output_file(self, output_file: {}):
+        self.fields = output_file
+
+    def __set_xpath_options(self, xpath_options: {}):
+        self.xpath_options = xpath_options
+
+    def __set_time_options(self, time_options: {}):
+        self.time_options = time_options
+
+    def __set_scroll_options(self, scroll_options: {}):
+        self.scroll_options = scroll_options
+
+    def __get_reviews_from_app(self, app_link: str) -> []:
+
+        reviews = []
+
+        app_link = self.__change_language_in_link(app_link)
+
+        # Step 1
+        self.driver.get(app_link)
+
+        # Step 2
+        app_name = self.driver.find_element(By.XPATH, self.xpath_options["app_name_xpath"]).text
+
+        # Step 3
+        review_link = self.driver.find_element(By.XPATH, self.xpath_options["review_link_xpath"])
+
+        self.__sleep(*self.time_options["delay_before_open_modal"])
+
+        # Step 4
+        self.__open_modal_window(review_link)
+
+        # Step 5
+        review_num = 1
+        while True:
+            review = self.__get_review_from_app(review_num)
+            review['app_name'] = str(app_name)
+            review['app_link'] = app_link
+            reviews.append(review)
+
+            if self.count_reviews <= review_num:
+                break
+
+            review_num = review_num + 1
+            self.__sleep(*self.time_options["delay_between_review"])
+        return reviews
+
+    def __get_review_from_app(self, review_num: int) -> {}:
+
+        review = {}
+
+        for field_name, value in self.regexp_xpath_options.items():
+            field = self.__find_review_field(review_num, field_name)
+
+            if field:
+                if field_name == "review_body":
+                    self.__scroll_to_review(field)
+                elif field_name == "rating":
+                    review[field_name] = field.get_attribute("aria-label")
+                else:
+                    review[field_name] = field.text
+            else:
+                review[field_name] = "None"
+
+        return review
+
+    def __find_review_field(self, review_num: int, field_name: str) -> WebElement:
+        try:
+            field = self.driver.find_element(By.XPATH, self.regexp_xpath_options[field_name] % (self.__main_xpath, review_num))
+
+            if field_name == "rating":
+                print("%s: %s" % (field_name, field.get_attribute("aria-label")))
+            elif field_name != "review_body":
+                print("%s: %s" % (field_name, field.text))
+            return field
+        except NoSuchElementException:
+            print("%s for review num - %d - has not been found!" % (field_name, review_num))
+            pass
+
+    def __open_modal_window(self, review_link: WebElement):
+        scroll_to_reviews = ScrollOrigin(review_link, 0, 0)
+        self.actions.scroll_to_element(review_link).perform()
+        self.actions.scroll_from_origin(scroll_to_reviews, 0, 500)
+        self.actions.move_to_element(review_link)
+        self.actions.click(review_link)
+        self.actions.perform()
+
+        WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, self.xpath_options["modal_window_name_xpath"])))
+
+    def __scroll_to_review(self, review_element: WebElement):
+        scroll_to_review = ScrollOrigin(
+            review_element,
+            random.randint(*self.scroll_options["scroll_origin_x_offset"]),
+            random.randint(*self.scroll_options["scroll_origin_y_offset"])
+        )
+        self.actions.scroll_from_origin(
+            scroll_to_review,
+            random.randint(*self.scroll_options["scroll_delta_x"]),
+            random.randint(*self.scroll_options["scroll_delta_y"])
+        ).move_to_element(review_element).perform()
+
+    def __change_language_in_link(self, link: str) -> str:
+        return re.sub(r'(.*hl=)(\D{2})(.*)?', (r'\1%s\3') % self.language, link)
+
+    def get_reviews(self) -> []:
+        return self.reviews
+
+    def run(self, app_links: []):
+        for app_link in app_links:
+            self.reviews += self.__get_reviews_from_app(app_link)
+
+        self.__sleep(*self.time_options["delay_before_close"])
+        self.driver.close()
+
+    @staticmethod
+    def __find_main_xpath(first_xpath: str, second_xpath: str) -> str:
+        if len(first_xpath) != len(second_xpath):
+            raise ValueError('XPATH for first and second reviews has different length! They must be the same length.')
+
+        position = 0
+        for key, letter in enumerate(first_xpath):
+            if letter != second_xpath[key]:
+                position = key
+                break
+
+        if not position:
+            raise ValueError('Have no a difference between first and second reviews xpath! '
+                             'They must have a different digits for some tag like "/html/div[1]" and "/html/div[2]".')
+
+        return first_xpath[:position - 1]
+
+    @staticmethod
+    def __sleep(min_time: int, max_time: int):
+        time.sleep(random.randint(min_time, max_time))
+
+    @staticmethod
     def clear_string(string: str) -> str:
         special_characters = [";", "\t", "\n", "\r", "\n\r", "<", ">"]
         return ''.join(filter(lambda i: i not in special_characters, string)).strip()
-
